a2b05e1a2bbdec425923219c964b2b15759e6ca9
Subhrosil Maiti <maitisubhrosil@gmail.com>
2020-08-16 02:40:28 +0530

    Create string_delimiter.c

---
+++ string_delimiter.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+int main() 
+{ 
+    char s[] = "my_name_is_edcast_future_skills"; 
+    int i = 0; 
+    scanf("%s", s); 
+    printf("\""); 
+    while(s[i] != '\0') 
+    { 
+        if(s[i] != '_')
+        {
+            printf("%c", s[i]); 
+            i++;
+        } 
+        else 
+        {
+            printf("\",\""); 
+            i++;
+        } 
+    } 
+    printf("\""); 
+    return 0; 
+}
